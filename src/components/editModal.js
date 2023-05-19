0b07338aec0f6e0ab3eb471af9699dd02c68d913
Jose Alcayaga <jose64am@aol.com>
2023-05-19 10:40:54 -0400

    added error message on text field, and basic error dialog template

--- src/components/editModal.js
+++ src/components/editModal.js
@@ -8,7 +8,7 @@ import {
     Dialog,
     DialogTitle,
     DialogContent,
-    DialogActions
+    DialogActions, DialogContentText
 } from "@mui/material";
 
 const marginProp = {margin:'10px'}
@@ -26,13 +26,32 @@ export default function EditModal({product, onClose})
                 <DialogTitle>Function</DialogTitle>
                 <DialogContent>
                     <Stack sx={marginProp}>
-                        <TextField sx={marginProp} label="Code" error></TextField>
+                        <TextField sx={marginProp} label="Code" error helperText="An Error"></TextField>
                         <TextField sx={marginProp} label="Name" error={false}></TextField>
                         <TextField sx={marginProp} label="Description" ></TextField>
                     </Stack>
                     <DialogActions>
                         <Button>Cancelar</Button>
                         <Button>Aceptar</Button>
+                    </DialogActions>
+                </DialogContent>
+            </div>
+        </Dialog>
+    )
+}
+
+function ErrorDialog({errorMessage, onClose})
+{
+    return(
+        <Dialog open={true} onClose={onClose}>
+            <div>
+                <DialogTitle>Error</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Ocurrió un error: {+""}
+                    </DialogContentText>
+                    <DialogActions>
+                        <Button onClose={onClose}>Aceptar</Button>
                     </DialogActions>
                 </DialogContent>
             </div>
